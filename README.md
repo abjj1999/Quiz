a05b5f2b63f1222260ffc8030cdbf470228101ae
abjj1999 <abdullahalhilfi21@gmail.com>
2021-07-27 00:24:40 -0500

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
 # Quiz
+
+# Questions
+ -How many legs does a spider have?
+ A. 4
+ B. 6
+ C. 8
+ D. 10
+
+ -What is the fastest land animal?
+ A. cheetah 
+ B. monkey
+ C. Dog
+ D. Cat
+
+-What is the largest planet in the solar system?
+ A. Earth 
+ B. Mars
+ C. Jupiter
+ D. Saturn
+
+ -What is the largest organ on the human body?
+ A. Liver 
+ B. Brain
+ C. Heart
+ D. Skin
+
+-Where is the White House located?
+ A. Texas 
+ B. Ohio
+ C. Wahington DC
+ D. Virginia
+
+-How many planets are in the solar system?
+A. 10
+B. 9
+C. 11
+D. 8
+
+{
+        Question: "What is the fastest land animal?",
+        Answer: {
+            a: 'cheetah',
+            b: 'monkey',
+            c: 'Dog',
+            d: 'Cat'
+        },
+        correct: 'A',
+
+    },
+    {
+        Question: "What is the largest planet in the solar system?",
+        Answer: {
+            a: 'Earth',
+            b: 'Mars',
+            c: 'Jupiter',
+            d: 'Saturn'
+        },
+        correct: 'C',
+
+    },
+    {
+        Question: "What is the largest organ on the human body?",
+        Answer: {
+            a: 'Liver',
+            b: 'Brain',
+            c: 'Heart',
+            d: 'Skin'
+        },
+        correct: 'D',
+
+    },
+    {
+        Question: "Where is the White House located?",
+        Answer: {
+            a: 'Texas',
+            b: 'Ohio',
+            c: 'Wahington DC',
+            d: 'Virginia'
+        },
+        correct: 'C',
+
+    },
+    {
+        Question: "How many planets are in the solar system?",
+        Answer: {
+            a: '10',
+            b: '9',
+            c: '11',
+            d: '8'
+        },
+        correct: 'A',
+
+    },
---
+++ assets/js/quiz.js
@@ -0,0 +1,131 @@
+// start Button 
+var startbtn = document.querySelector(".start-btn");
+startbtn.addEventListener("click", function() {
+    var homePage = document.getElementById("home");
+    homePage.style.display = "none";
+    var quizPage = document.querySelector(".hide");
+    quizPage.style.display = "flex";
+})
+
+
+var Quiz = [
+    {
+        question: "What is the largest planet in the solar system?",
+        a: 'Earth',
+        b: 'Mars',
+        c: 'Jupiter',
+        d: 'Saturn',
+        correct: "c"
+    },
+    {
+        question: "What is the fastest land animal?",
+        a: 'cheetah',
+        b: 'monkey',
+        c: 'Dog',
+        d: 'Cat',
+        correct: "a"
+    },
+    {
+        question: "What is the largest organ on the human body?",
+        a: 'Liver',
+        b: 'Brain',
+        c: 'Heart',
+        d: 'Skin',
+        correct: "d"
+    },
+    {
+        question: "How many planets are in the solar system?",
+        a: '10',
+        b: '8',
+        c: '11',
+        d: '9',
+        correct: "b"
+    },
+];
+
+var currentQ = 0;
+var score = 0;
+
+const questionEl = document.querySelector(".text")
+const a_text = document.querySelector("#a_text");
+const b_text = document.querySelector("#b_text");
+const c_text = document.querySelector("#c_text");
+const d_text = document.querySelector("#d_text");
+const btn = document.querySelector(".submit");
+const numQ = document.querySelector(".numQ");
+const answerEls = document.querySelectorAll(".input-css");
+const timer = document.querySelector(".time");
+
+function countdown() {
+    var timeleft = 30;
+
+    var timeInterval = setInterval(function(){
+        if(currentQ < Quiz.length){
+            timer.textContent = timeleft;
+            timeleft--;
+        }
+        else{
+            clearInterval(timeInterval);
+        }
+        
+    }, 1000)
+}
+
+var loadQuiz = function(){
+    clear();
+    const currentQD = Quiz[currentQ];
+    const index = currentQ + 1;
+    numQ.textContent = index;
+    questionEl.textContent = currentQD.question;
+    a_text.textContent = currentQD.a;
+    b_text.textContent = currentQD.b;
+    c_text.textContent = currentQD.c;
+    d_text.textContent = currentQD.d;
+    
+    
+}
+loadQuiz();
+
+var getSelected = function(){
+    var answer = undefined;
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked){
+            answer =  answerEl.id;
+        }
+    })
+    return answer;
+}
+
+function clear(){
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+
+}
+
+
+btn.addEventListener("click", function() {
+    const ans = getSelected();
+    
+    if(ans){  
+        if(ans === Quiz[currentQ].correct){
+            score++;
+            
+        }
+        
+        currentQ++;
+        if(currentQ < Quiz.length){
+            loadQuiz();
+        }
+        else{
+            alert("you done")
+        }
+    }
+    
+})
+
+
+startbtn.onclick = countdown;
+
+
+
